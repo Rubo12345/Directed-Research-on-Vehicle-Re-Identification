3bfd5494756eed545961ce397fbb8fb9c0d1b040
Rutwik <rrbonde@wpi.edu>
2022-04-11 10:38:42 -0400

    Preprocessing and ResNet

--- SLB_Train.py
+++ SLB_Train.py
@@ -1,5 +1,10 @@
+from fileinput import filename
+from multiprocessing import BoundedSemaphore
 import numpy as np
 import torch
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import os.path as osp
 from PIL import Image
 import sys
@@ -7,7 +12,6 @@ sys.path.append('/home/rutu/WPI/Directed_Research/My_Approach_To_DR')
 from Datasets import veri_train, Rotation
 
 V = veri_train.VeRi()
-
 dataset_dir = '/home/rutu/WPI/Directed_Research/ReID_Datasets/VeRi'
 train_dir = osp.join(dataset_dir, 'image_train')
 train_list = None   # I think data gets shuffled using this  # 37778
@@ -29,21 +33,18 @@ def image_to_pixel(image):
 
 def input_to_4d_tensor(I):
     Tensor = torch.Tensor(I)
-    # Tensor = torch.reshape(Tensor, (1,3,156,156))
-    Tensor = torch.reshape(Tensor,(1,3,int(np.sqrt(len(I)/3)),int(np.sqrt(len(I)/3))))   #check this
+    Tensor = torch.reshape(Tensor,(1,3,Tensor.shape[0],Tensor.shape[1]))   #check this
     return Tensor
 
 def Data_Rotation(Train_Images):
     Dsl = []
-    for i in range(1):
-        image = Image.open(Train_Images[i])
-        # image = Image.open("/home/rutu/WPI/Directed_Research/ReID_Datasets/VeRi/image_train/0496_c017_00035340_0.jpg")
-        image_to_pixel = image_to_pixel(image)
-        input_to_4d_tensor = input_to_4d_tensor(image_to_pixel)
-        R_0 = Rotation._apply_2d_rotation(input_to_4d_tensor,0)
-        R_90 = Rotation._apply_2d_rotation(input_to_4d_tensor,90)
-        R_180 = Rotation._apply_2d_rotation(input_to_4d_tensor,180)
-        R_270 = Rotation._apply_2d_rotation(input_to_4d_tensor,270)
+    for i in range(100):
+        image = mpimg.imread(Train_Images[i])
+        _4d_tensor = input_to_4d_tensor(image)
+        R_0 = Rotation._apply_2d_rotation(_4d_tensor,0)
+        R_90 = Rotation._apply_2d_rotation(_4d_tensor,90)
+        R_180 = Rotation._apply_2d_rotation(_4d_tensor,180)
+        R_270 = Rotation._apply_2d_rotation(_4d_tensor,270)
         Dsl.append(R_0)
         Dsl.append(R_90)
         Dsl.append(R_180)
@@ -51,27 +52,37 @@ def Data_Rotation(Train_Images):
     return Dsl
 
 Train_Images, Train_Labels, Train_Cams = data_image_labels(train_dir, train_list)
+Dsl = Data_Rotation(Train_Images)
 
-# image = Image.open("/home/rutu/WPI/Directed_Research/ReID_Datasets/VeRi/image_train/0496_c017_00035340_0.jpg")
-# # print(image_to_pixel(image))
-# I = image_to_pixel(image)
-# I = input_to_4d_tensor(I)
-# R = Rotation._apply_2d_rotation(I,90)
-# print(R)
+'''#ResNet
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+# or any of these variants
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet101', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
+model.eval()
+print(Train_Images[0])
+filename = Train_Images[0]
+filename = Dsl[0]
+# input_image = Image.open(filename)
+input_image = Dsl[0]
+print(input_image.shape)
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+print(input_batch.shape)
 
-Dsl = []
-for i in range(1):
-    # image = Image.open(Train_Images[i])
-    image = Image.open("/home/rutu/WPI/Directed_Research/ReID_Datasets/VeRi/image_train/0496_c017_00035340_0.jpg")
-    image_to_pixel = image_to_pixel(image)
-    input_to_4d_tensor = input_to_4d_tensor(image_to_pixel)
-    R_0 = Rotation._apply_2d_rotation(input_to_4d_tensor,0)
-    R_90 = Rotation._apply_2d_rotation(input_to_4d_tensor,90)
-    R_180 = Rotation._apply_2d_rotation(input_to_4d_tensor,180)
-    R_270 = Rotation._apply_2d_rotation(input_to_4d_tensor,270)
-    Dsl.append(R_0)
-    Dsl.append(R_90)
-    Dsl.append(R_180)
-    Dsl.append(R_270)
-print(Dsl)
-
+with torch.no_grad():
+    output = model(input_batch)
+# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+# print(output[0])
+# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+probabilities = torch.nn.functional.softmax(output[0], dim=0)
+print(probabilities)
+print(len(probabilities))
+'''
